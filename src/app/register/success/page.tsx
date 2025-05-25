dc431770c85fdd71c0b9bbd3e7d9ed499a5dc5db
aangel-sama <aapaillan@uc.cl>
2025-05-25 15:35:33 -0400

    Register -> success

---
+++ src/app/register/success/page.tsx
@@ -0,0 +1,43 @@
+// src/app/register/success/page.tsx
+'use client'
+
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+
+export default function RegisterSuccessPage() {
+  const router = useRouter()
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8 bg-white">
+      {/* Logo */}
+      <Image
+        src="/logo_cyd.png"
+        alt="Logo CyD"
+        width={100}
+        height={100}
+      />
+
+      <div className="w-full max-w-lg p-6 shadow-lg bg-white text-center">
+        {/* Mensaje de éxito */}
+        <div className="flex items-center justify-center mb-2 text-lg font-medium text-gray-900">
+          <Image
+            src="/check.png"
+            alt="Check"
+            width={16}
+            height={16}
+            className="mr-2"
+          />
+          ¡Éxito!
+        </div>
+        <div className="mb-5 text-sm text-gray-600">
+          El registro se completó correctamente
+        </div>
+        <button
+          onClick={() => router.push('/login')}
+          className="w-full py-3 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 transition"
+        >
+          Iniciar sesión
+        </button>
+      </div>
+    </div>
+  )
+}
